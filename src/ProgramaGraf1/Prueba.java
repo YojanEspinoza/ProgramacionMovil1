50c683deb9380a374ba8311c493e13b49eb855d6
YojanEspinoza <yojanespinoza18@aragon.unam.mx>
2025-11-09 09:12:45 -0600

    Programas 9-Noviembre-2025

--- src/ProgramaGraf1/Prueba.java
+++
@@ -1,9 +0,0 @@
-//Espinoza Flores Yojan Gael
-package ProgramaGraf1;
-
-public class Prueba {
-    public static void main(String []args){
-        Manejador m = new Manejador();
-        m.setVisible(true);//Hace visible la ventana
-    }
-}
--- src/ProgramaGraf2/Prueba.java
+++
@@ -1,9 +0,0 @@
-//Espinoza Flores Yojan Gael
-package ProgramaGraf2;
-
-public class Prueba {
-    public static void main(String[]args){
-        EjemploG eg = new EjemploG();
-        eg.setVisible(true);
-    }
-}
